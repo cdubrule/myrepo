87c09553ff9b755203296a0ae9d80aff95f8a120
Craig Dubrule <cjdubrule@gmail.com>
2013-08-14 18:21:04 +0000

    web.js

--- web.js
+++ web.js
@@ -1,18 +1,10 @@
-var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//fs.readFile('index.html',function(err,data){
-//if (err) throw err;
-//console.log(data);
-//});
-
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World from index.html');
 });
-
-//console.log('Hi there');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
